cb892eaa5b287faf57f0f0d6116e995bb905eb10
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2021-11-10 21:28:24 -0300

    primeiros passos em python

---
+++ Untitled.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World!")
+
+
+# In[2]:
+
+
+x = 5
+
+
+# In[3]:
+
+
+print(x)
+
+
+# In[4]:
+
+
+print(type(x))
+
+
+# In[19]:
+
+
+preco = 19.99
+print(preco,type(preco))
+
+
+# In[6]:
+
+
+preco = 19.99
+print(preco,type(preco))
+
+
+# In[7]:
+
+
+cidade = "São Paulo"
+print(cidade, type(cidade))
+
+
+# In[8]:
+
+
+cidade = 'cidade'
+print(cidade)
+
+
+# In[9]:
+
+
+cidade = 'sp'
+print(cidade)
+
+
+# In[10]:
+
+
+disponivel = True
+print(disponivel)
+
+
+# In[12]:
+
+
+x = 50
+y = 2
+
+
+# In[13]:
+
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+
+# In[14]:
+
+
+print(x ** y)
+print(x // y)
+print(x % y)
+
+
+# In[15]:
+
+
+tem_cafe = True
+tem_pao = False
+
+
+# In[16]:
+
+
+print(not tem_cafe)
+print(tem_cafe or tem_pao)
+print(tem_cafe and tem_pao)
+
+
+# In[17]:
+
+
+dolar = 5.3
+real = 1
+
+
+# In[18]:
+
+
+print(dolar > real)
+print(dolar < real)
+print(dolar == real)
+print(dolar >= real)
+print(dolar <= real)
+print(dolar != real)
+
+
+# In[ ]:
+
+
+
+
